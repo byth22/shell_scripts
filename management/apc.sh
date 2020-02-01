8971c837846fedbb56914b4bba841c00bc2b923b
byth22 <kellvinps@gmail.com>
2020-02-01 15:37:19 -0300

    Create apc.sh

---
+++ management/apc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "-- Automatic Protocol Changing"
+
+function udp_tcp () {
+        #sed -i -e 's/udp/tcp/g; s/1337/443/g; s/tls_auth/tls_crypt/g' *.ovpn
+
+        echo "  [+] Troca feita para TCP"
+}
+
+function tcp_udp () {
+        #sed -i -e 's/tcp/udp/g; s/443/1337/g; s/tls_crypt/tls_auth/g' *.ovpn
+        echo "  [+] Troca feita para UDP"
+}
+
+function error () {
+        echo "  [!] Opção inválida"
+        echo "  [!] Usage: ./apc.sh [tcp/udp]"
+}
+
+
+if [ $# -ne 1 ]; then
+        error
+        exit
+fi
+
+if [ $1 == "tcp" ]; then
+        udp_tcp
+
+elif [ $1 == "udp" ]; then
+        tcp_udp
+
+else
+        error
+fi
